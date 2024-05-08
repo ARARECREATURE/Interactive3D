311f37260013ee0eb1ddb395871f1a40f22d7d72
Hector-Deng <1391812054@qq.com>
2024-05-08 22:08:05 +1000

    Reset character,Terrain and Dragon

---
+++ Interactive3DProject/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float translationSpeed = 2.0f;
+        float rotationSpeed = 150f;
+        private Animator animator;
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float translation = Input.GetAxis("Vertical") * translationSpeed;
+            translation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
+
+            float rotation= Input.GetAxis("Horizontal") * rotationSpeed;
+            rotation *= Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+            {
+                animator.SetBool("isWalk", true);
+
+
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+            {
+                animator.SetBool("isWalk", false);
+            }
+
+            animator.SetBool("isRun", Input.GetKey(KeyCode.LeftShift));
+
+
+
+
+
+        
+        }
+}
